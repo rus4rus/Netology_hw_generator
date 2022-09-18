d7cccfb80afad0c225f191ed74047ad00197da3c
Ruslan Shakirov <shakirov3000@mail.ru>
2022-09-18 21:15:24 +0500

    init

---
+++ main.py
@@ -0,0 +1,33 @@
+#
+#     Написать генератор, который принимает список списков, и возвращает их плоское представление. Например
+#
+# nested_list = [
+# 	['a', 'b', 'c'],
+# 	['d', 'e', 'f'],
+# 	[1, 2, None],
+# ]
+# for item in  flat_generator(nested_list):
+# 	print(item)
+
+def flat_generator(list_: list):
+	i = -1
+	k = -1
+	while True:
+		if len(list_) == i+1:
+			break
+		i += 1
+		while True:
+			if len(list_[i]) == k+1:
+				k = -1
+				break
+			k += 1
+			yield list_[i][k]
+
+# nested_list = [
+# 	['a', 'b', 'c'],
+# 	['d', 'e', 'f'],
+# 	[1, 2, None],
+# ]
+nested_list = []
+for item in  flat_generator(nested_list):
+	print(item)
